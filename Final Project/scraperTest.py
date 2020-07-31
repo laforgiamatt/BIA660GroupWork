76815f45ea6dbd5d307b4e14cbe426a77d914755
VinothkumarK24 <vinothkkumar24@gmail.com>
2020-07-30 23:29:14 -0400

    unit testing

--- Final Project/scraperTest.py
+++ Final Project/scraperTest.py
@@ -1,11 +1,20 @@
 """
 unit tests for scraper
 """
-
 #import response
 import unittest
-
+import requests
+import csv
+import os.path
+import re
+import time
+from bs4 import BeautifulSoup
+from selenium import webdriver
 import scraper as myScraper
+
+dataFilename='trainingCards.txt'
+url = myScraper.randomLinkGenerator()
+response = myScraper.requestBuilder(url)
 
 def checkDataParsedTest():
     assert myScraper.checkDataParsed('authors.txt') == True
@@ -14,27 +23,50 @@ def requestBuilderTest():
     assert myScraper.requestBuilder('http://www.google.com').status_code == 200
 
 def driverBuilderTest():
-    #TODO
-    #Assert Driver is built
+#   TODO
+#   Assert Driver is built
+    driver.get('https://www.w3schools.com/html/html_examples.asp')
+    print(driver.title)
+    assert driver.title == 'HTML Examples'
 
 def randomLinkGeneratorTest():
     #TODO
     #Assert Link exists
+    assert myScraper.randomLinkGenerator()=="https://scryfall.com/random"
 
 def myStripTest():
     #TODO
+    htmltxt = "<p>Hello World</p>"
+    soup = BeautifulSoup(htmltxt, 'lxml')
+    assert myScraper.myStrip(soup) == 'Hello World'
 
 def scrapeCardTest():
-    #TODO
+   #TODO
+    cardData = myScraper.scrapeCard(response.text)
+    assert len(cardData['cardName'])>1
+    assert len(cardData['cardType'])>1
+    assert len(cardData['cardText'])>1
 
 def csvWriterTest():
-    #TODO
+   #TODO
+    myScraper.csvWriter(cardData, dataFilename)
+    assert myScraper.checkDataParsed(dataFilename)==True
 
 def runTest():
-    assert True
+#     myScraper.run()
+    fname = 'trainingCards.txt'
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    no_of_lines_count =i + 1
+    assert no_of_lines_count > 1
 
 if __name__ == "__main__":
     checkDataParsedTest()
     requestBuilderTest()
+    randomLinkGeneratorTest()
+    myStripTest()
+    scrapeCardTest()
+    csvWriterTest()
     runTest()
     print("Tests all passed")
