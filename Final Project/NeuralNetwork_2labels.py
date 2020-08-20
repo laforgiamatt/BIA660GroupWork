acc08586ffb145f706667c2575196784cf96ff9d
Matthew LaForgia <mlaforgia2@gmail.com>
2020-08-20 04:11:11 -0400

    more card data points and updating neuralnetwork2labels to work with rest of software

--- Final Project/NeuralNetwork_2labels.py
+++ Final Project/NeuralNetwork_2labels.py
@@ -34,18 +34,19 @@ TAG_RE = re.compile(r'<[^>]+>')
 def remove_tags(text):
     return TAG_RE.sub('', text)
 
-def model(X):
+def model(X=None):
     
     df = SA2.run()
     sns.countplot(x='Decisions', data=df)
 
-    
+    X = []
     sentences = list(df['Carddata'])
     for sen in sentences:
         X.append(preprocess_text(sen))
 
     y = df['Decisions']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.20, random_state=42)
+    tokenizer = Tokenizer(num_words=10000)
     tokenizer.fit_on_texts(X_train)
 
     X_train = tokenizer.texts_to_sequences(X_train)
@@ -58,7 +59,7 @@ def model(X):
 
 
     embeddings_dictionary = dict()
-    glove_file = open('/Users/vinodhkumar/Downloads/glove.42B.300d.txt', encoding="utf8")
+    glove_file = open('glove.42B.300d.txt', encoding="utf8")
 
     for line in glove_file:
         records = line.split()
@@ -86,20 +87,25 @@ def model(X):
     print("Score:", score[0])
     print("Test Accuracy:", score[1])
     return model
+
+def run():
+    model()
+
 # Testing:
-tokenizer = Tokenizer(num_words=5000)
-X = []
-model = model(X)
-test = X[4]
-print(test)
-test = tokenizer.texts_to_sequences(test)
-sentlist = []
-for sublist in test:
-    for item in sublist:
-        sentlist.append(item)
+def testingNN():
+    tokenizer = Tokenizer(num_words=5000)
+    X = []
+    model = model(X)
+    test = X[4]
+    print(test)
+    test = tokenizer.texts_to_sequences(test)
+    sentlist = []
+    for sublist in test:
+        for item in sublist:
+            sentlist.append(item)
 
-sentlist = [sentlist]
+    sentlist = [sentlist]
 
-test = pad_sequences(sentlist, padding='post', maxlen=300)
+    test = pad_sequences(sentlist, padding='post', maxlen=300)
 
-print(model.predict(test))
+    print(model.predict(test))
